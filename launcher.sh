029d666bd04359198d397b048d8d1e864e40dc2d
nicolas <nicolachampy@gmail.com>
2026-02-07 10:53:07 +0100

    feat: add launcher.sh

---
+++ launcher.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Attendre que Wayland/X11 soit initialisé
+while [ -z "$WAYLAND_DISPLAY" ] && [ -z "$DISPLAY" ]; do
+  sleep 1
+done
+
+# Aller dans le dossier du projet
+cd /home/nicolas/Téléchargements/MiniOS/src
+# Lancer l'app
+/usr/bin/python3 Test.py
